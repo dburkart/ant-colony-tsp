f06d88cac162a17f42a5f03cd42d136cc1c436bf
Bob Schriver <bobschriver@gmail.com>
2011-10-30 18:27:06 -0400

    Nearest Neighbor tour working

--- do_tsp_tour.m
+++ do_tsp_tour.m
@@ -7,11 +7,13 @@ function [ tour , distance] = do_tsp_tour( cities )
 
     pheromone_matrix = zeros(size(cities));
 
-    visited_cities = zeros([number_of_ants number_of_cities + 1]);
+    visited_cities = zeros([number_of_ants number_of_cities]);
 
     tour_distance = zeros([number_of_ants , 1]);
 
-    [nearest_neighbor_tour , nearest_neighbor_tour_distance] = do_nearest_neighbor_tour( cities );
+    [nearest_neighbor_tour , nearest_neighbor_tour_distance] = do_nearest_neighbor_tour( cities )
+    
+    
 
     
     initial_pheromone_level = 1 / (nearest_neighbor_tour_distance * number_of_cities);
@@ -22,7 +24,12 @@ function [ tour , distance] = do_tsp_tour( cities )
         for k = 1:number_of_ants
             %Get the city which is stored in the previous location, ie the
             %city we are in now
-            current_city = visited_cites(k , n - 1);
+            current_city = visited_cities(k , n - 1)
+            
+            if current_city == 0
+                current_city = n - 1
+            end
+                
             neighboring_adjacency = cities(current_city);
         
         
@@ -31,7 +38,7 @@ function [ tour , distance] = do_tsp_tour( cities )
             next_city = current_city;
         
             while size(find(visited_cities(k) == next_city)) > 0
-                next_city = round(rand(1) * number_of_cities);
+                next_city = round(rand(1) * number_of_cities)
             end
         
             visited_cities(k , n) = next_city;
@@ -52,12 +59,12 @@ function [ tour , distance] = do_tsp_tour( cities )
         
     end
 
-    for k = 1:number_of_ants:
+    for k = 1:number_of_ants
         visited_cities(k , number_of_cities + 1) = visited_cities(k , 1);
         current_city = visited_cites(k , number_of_cities)
     
         %Calculate the distance between the current city and starting city
-        tour_distance(k) = tour_distance(k) + cities(current_city  , visited_cites(k , 1);
+        tour_distance(k) = tour_distance(k) + cities(current_city  , visited_cites(k , 1));
     end
               
     [shortest_tour_distance , best_ant] = min(tour_distance)
@@ -97,21 +104,34 @@ function [nn_tour , nn_tour_length] = do_nearest_neighbor_tour( cities )
     
     current_city = 1
     
-    nn_tour = zeros(number_of_cities);
+    nn_tour = zeros(number_of_cities , 1);
+    nn_tour_length = 0
     
-    for k = 1:number_of_cities
-        neighboring_cities = cities(current_city);
+    for k = 1:(number_of_cities - 1)
+        neighboring_cities = cities(current_city , :)
+        neighboring_cities(current_city) = [];
         
-        [nearest_distance , nearest_neighbor] = min(neighboring_cities);
         
-        while size(find(nn_tour == nearest_neighbor)) > 0
-            neighboring_cities(nearest_neighbor) = [];
+        [nearest_distance , nearest_neighbor] = min(neighboring_cities)
+        index = size(find(nn_tour == nearest_neighbor) , 1)
+        tour = nn_tour
+        
+        
+        while size(find(nn_tour == nearest_neighbor) , 1)
+            %We cant remove the nearest neighbor or the indices get messed
+            %up, so we simply make it not the min
+            neighboring_cities(nearest_neighbor) = intmax('uint32');
             [nearest_distance , nearest_neighbor] = min(neighboring_cities);
         end
         
         nn_tour(k) = nearest_neighbor;
-        nn_tour_length = nearest_distance;
+        nn_tour_length = nn_tour_length + nearest_distance;
         
         current_city = nearest_neighbor;
     end
+    
+    nn_tour(number_of_cities) = 1
+    nn_tour_length = nn_tour_length + cities(current_city , 1)
+    
+    
 end
