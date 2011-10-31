03920558fa29f790cf6f0aed4b1b20ef55e5384f
Kate Dubuisson <kxd8885@rit.edu>
2011-10-31 12:15:41 -0400

    Only update edges belonging to best ant

--- global_update.m
+++ global_update.m
@@ -1,6 +1,6 @@
 function [ shortest_tour ] = global_update( cities )
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
+%global_update.m
+%   Global update phase of TSP, update only edges belonging to best ant
     number_of_ants = 10;
     a = .1
 
@@ -8,9 +8,16 @@ function [ shortest_tour ] = global_update( cities )
     [tour, distance] = do_tsp_tour(cities);
     tour_length = size(tour);
     
-    for k = 1:tour_length
-        previous_city = visited_cities(k , n - 1);
-        current_city = visited_cities(k , n);
+    % determine the best ant from shortest tour
+    for k = 1:number_of_ants
+        if size(find(visited_cities(k) == tour)) > 0
+            best_ant = k
+        end
+    end
+    
+    for n = 2:tour_length
+        previous_city = visited_cities(best_ant , n - 1);
+        current_city = visited_cities(best_ant , n);
         
         current_pheromone = pheromone_matrix(previous_city , current_city);
         
