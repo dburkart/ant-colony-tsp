8e526f8c233d4ef1ab66958ec1b12a48c2e09e77
Bob Schriver <bobschriver@gmail.com>
2011-11-02 02:08:48 -0400

    Tour creation works with distance measure

--- do_tsp_tour.m
+++ do_tsp_tour.m
@@ -5,13 +5,15 @@ function [ tour , distance] = do_tsp_tour( cities )
     number_of_ants = 10;
     number_of_cities = size(cities , 1);
 
-    pheromone_matrix = zeros(size(cities));
+    pheromones = ones(size(cities));
 
     visited_cities = zeros([number_of_ants number_of_cities]);
 
     tour_distance = zeros([number_of_ants 1]);
 
-    [nearest_neighbor_tour , nearest_neighbor_tour_distance] = do_nearest_neighbor_tour( cities );
+    [nearest_neighbor_tour , nearest_neighbor_tour_distance] = do_nearest_neighbor_tour( cities )
+    
+    
     
     %Generate the ants original starting location
     visited_cities(: , 1) = floor(rand(1 , number_of_ants) * number_of_cities) + 1;
@@ -19,6 +21,8 @@ function [ tour , distance] = do_tsp_tour( cities )
     
     initial_pheromone_level = 1 / (nearest_neighbor_tour_distance * number_of_cities);
     p = .1
+    pheromones =  pheromones * initial_pheromone_level;
+    
     %TODO Generate starting posistions
 
     for n = 2:number_of_cities
@@ -34,13 +38,18 @@ function [ tour , distance] = do_tsp_tour( cities )
         
             %Need to calculate our next city here
             %TODO replace random search with actual search
-            next_city = current_city;
+            %next_city = current_city;
         
             
             %Keep generating randomly until 
-            while size(find(visited_cities(k , :) == next_city)) > 0
-                next_city = floor(rand(1) * number_of_cities) + 1;
-            end
+            %while size(find(visited_cities(k , :) == next_city)) > 0
+             %   next_city = floor(rand(1) * number_of_cities) + 1;
+            %end
+            neighboring_cities = 1:number_of_cities;
+                      
+            unvisited_cities = setdiff(neighboring_cities , visited_cities(k , :));
+            
+            [next_city , weight] = probability_between_cities(current_city , cities(current_city , :) , pheromones(current_city , :) , unvisited_cities);
         
             visited_cities(k , n) = next_city;
             tour_distance(k , 1) = tour_distance(k , 1) + neighboring_adjacency(next_city);       
@@ -53,9 +62,9 @@ function [ tour , distance] = do_tsp_tour( cities )
             previous_city = visited_cities(k , n - 1);
             current_city = visited_cities(k , n);
         
-            current_pheromone = pheromone_matrix(previous_city , current_city);
+            current_pheromone = pheromones(previous_city , current_city);
         
-            pheromone_matrix(previous_city , current_city) = (1 - p) * current_pheromone + p * initial_pheromone_level;
+            pheromones(previous_city , current_city) = (1 - p) * current_pheromone + p * initial_pheromone_level;
         end
         
     end
@@ -70,34 +79,39 @@ function [ tour , distance] = do_tsp_tour( cities )
               
     [shortest_tour_distance , best_ant] = min(tour_distance)
 
-    tour = visited_cities(best_ant);
+    tour = visited_cities(best_ant , :);
     distance = shortest_tour_distance;
 
 end
 
-function [ probability ] = probability_between_cities( city_r , city_s , cities , pheremones , unvisited_cities )
-    b = .5
+function [ max_city , max_weight] = probability_between_cities(city_r , cities_r , pheromones_r , unvisited_cities )
+    b = .5;
 
-    distance = cities(city_r , city_s);
-    pheremone = pheremones(city_r , city_s);
+    %distance = cities(city_r , city_s);
+    %pheremone = pheremones(city_r , city_s);
     
-    city_weight = distance * pheremone ^ b;
+    %city_weight = distance * pheremone ^ b;
     
-    unvisited_sum = 0
+    %unvisited_sum = 0
     
-    for i = 1:size(unvisited_cities)
-        unvisited_city = unvisited_cities(i);
+    max_weight = 0;
+    
+    max_city = unvisited_cities(1);
         
-        unvisited_distance = cities(city_r , unvisited_city);
-        unvisited_pheremone = pheremones(city_r , unvisited_city);
+    for unvisited_city = unvisited_cities
         
-        unvisited_weight = unvisited_distance * unvisited_pheremone ^ b;
+        unvisited_distance = cities_r(unvisited_city);
+        unvisited_pheromone = pheromones_r(unvisited_city);
         
-        unvisited_sum = unvisited_sum + unvisited_weight;
-    end
-    
-    probability = city_weight / unvisited_sum;
-    
+        unvisited_weight = (1 / unvisited_distance) * unvisited_pheromone ^ b;
+        
+        if unvisited_weight > max_weight
+            max_weight = unvisited_weight;
+            max_city = unvisited_city;
+        end
+        
+    end 
+        
 end
 
 function [nn_tour , nn_tour_length] = do_nearest_neighbor_tour( cities )
@@ -106,7 +120,7 @@ function [nn_tour , nn_tour_length] = do_nearest_neighbor_tour( cities )
     current_city = 1;
     
     nn_tour = zeros(number_of_cities , 1);
-    nn_tour_length = 0
+    nn_tour_length = 0;
     
     for k = 1:(number_of_cities - 1)
         neighboring_cities = cities(current_city , :);
